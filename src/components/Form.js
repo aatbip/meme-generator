c585ecd9ee17dd93abbd102a5d4a9bcfd19caa0f
aatbip <anantabipal2010@gmail.com>
2022-12-28 20:04:30 +0545

    final commit

---
+++ src/components/Form.js
@@ -0,0 +1,92 @@
+import React from "react";
+import data from "../memesData";
+
+let url;
+
+function randomNumber() {
+  let x = Math.floor(Math.random() * 100 + 1);
+  return x;
+}
+
+let api = "https://api.imgflip.com/get_memes";
+
+export const Form = () => {
+  let [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState([]);
+  const inputReference = React.useRef(null);
+
+  function handleChange(event) {
+    const { value, name, type } = event.target;
+    setMeme((memes) => {
+      return {
+        ...memes,
+        [name]: value,
+      };
+    });
+  }
+
+  React.useEffect(() => {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => setAllMemeImages(data.data.memes));
+  }, [allMemeImages]);
+
+  React.useEffect(() => {
+    inputReference.current.focus();
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    url = allMemeImages[randomNumber()].url;
+    setMeme((prevData) => {
+      return {
+        ...prevData,
+        randomImage: url,
+      };
+    });
+  }
+
+  return (
+    <div className="form">
+      <div className="form-wrapper">
+        <form>
+          <input
+            id="input-1"
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            onChange={handleChange}
+          />
+          <input
+            id="input-2"
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            onChange={handleChange}
+          />
+        </form>
+        <div
+          onClick={handleClick}
+          className="form-button hvr-grow"
+          name="randomImage"
+          type="button"
+          ref={inputReference}
+        >
+          Get a new meme image 🖼
+        </div>
+      </div>
+      <div className="image">
+        <div className="image-wrapper">
+          <img src={meme.randomImage} alt="this is a meme image" />
+          <p id="top-text">{meme.topText}</p>
+          <p id="bottom-text">{meme.bottomText}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-wrapper">
+        <div className="header-logo">
+          <img src="/logo.png" alt="meme generator logo" />
+          <h1>Meme Generator For Maya</h1>
+        </div>
+        <p>React Course - Project 3</p>
+      </div>
+    </div>
+  );
+};
